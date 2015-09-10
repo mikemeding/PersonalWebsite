2e25e986b47b3c18def62749e3ce873ed331835b
mikemeding <mikeymeding@gmail.com>
2015-09-10 12:40:17 -0400

    finished all static pages. deploying now.

--- public/app/indexController.js
+++ public/app/indexController.js
@@ -4,9 +4,9 @@
 
 var app = angular.module('mainSite');
 // main page controller
-app.controller('MainSiteController', ["$http", "$scope", "Lightbox", MainSiteController])
+app.controller('MainSiteController', ["$http", "$scope", MainSiteController])
 
-function MainSiteController($http, $scope, Lightbox) {
+function MainSiteController($http, $scope) {
 
     $(function () {
         angular.element('[data-toggle="tooltip"]').tooltip();
@@ -15,92 +15,5 @@ function MainSiteController($http, $scope, Lightbox) {
     $scope.title = "";
     $scope.stuff = [];
 
-    function getPosts() {
-        $scope.title = "";
-        var request = {
-            method: 'GET',
-            url: 'http://localhost:3000/getAllPosts'
-        };
-        $http(request)
-            .success(function (data) { // If call successful
-                $scope.stuff = data;
-            })
-            .error(function () { // If call fails
-                console.error("failed to get posts");
-            });
-    }
-
-    getPosts(); // on page load
-
-    $scope.addNewPost = function (title) {
-        var request = {
-            method: 'POST',
-            url: 'http://localhost:3000/addPost',
-            data: {
-                title: title
-            }
-        };
-        $http(request)
-            .success(function (data) { // If call successful
-                console.log("added some new data");
-                getPosts();
-            })
-            .error(function () { // If call fails
-                console.error("failed to add new item");
-            });
-    };
-
-    $scope.removePost = function (post) {
-        //console.log(post);
-        var request = {
-            method: 'POST',
-            url: 'http://localhost:3000/removePost',
-            data: {
-                post: post
-            }
-        };
-        $http(request)
-            .success(function (data) { // If call successful
-                console.log("removed data");
-                getPosts();
-            })
-            .error(function () { // If call fails
-                console.error("failed to remove item");
-            });
-    };
-
-    /**
-     * get the file list of the img directory
-     */
-    $scope.fileList = [];
-    $scope.getAllPhotos = function () {
-        var request = {
-            method: 'GET',
-            url: 'http://localhost:3000/getListOfPhotos/'
-        };
-
-        $http(request)
-            .success(function (data) { // If call successful
-
-                // push each image into a specific format for lightbox
-                angular.forEach(data, function (value, key) {
-                    var url = "../img/" + value;
-                    $scope.fileList.push({"url": url});
-                });
-                console.log($scope.fileList);
-
-            })
-            .error(function () { // If call fails
-                console.error("unable to get file list");
-            });
-    };
-    $scope.getAllPhotos();
-
-    /**
-     * open the lightbox modal when clicked on
-     */
-    $scope.openLightbox = function (index) {
-        Lightbox.openModal($scope.fileList, index);
-    };
 
 }
